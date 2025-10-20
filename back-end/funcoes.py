30c1ccd7a721a292c6b515f4a64fb664f7543509
Kazuki Vitor Shiroma <kazukishiroma06@gmail.com>
2025-10-20 09:33:03 -0300

    Função adicionar produtos

--- back-end/funcoes.py
+++ back-end/funcoes.py
@@ -6,7 +6,7 @@ def criar_tabela():
         try:
             cursor.execute("""
         CREATE TABLE produtos (
-        id INT AUTO_INCREMENT PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         nome VARCHAR(100) NOT NULL,
         categoria VARCHAR(50),
         preco DECIMAL(10,2),
@@ -20,3 +20,14 @@ def criar_tabela():
             cursor.close()
             conexao.close()
 criar_tabela()
+
+def adicionar_produto(nome, categoria, preco, quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute("""
+            INSERT INTO produtos (nome, categoria, preco, quantidade)
+            VALUES (%s, %s, %s, %s)
+            """), (nome, categoria, preco, quantidade)
+        except Exception as error:
+            print(f"Erro: {error}")
